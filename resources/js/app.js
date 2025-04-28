25bdb508982366b1b884d435feefcc81afcbdf13
bagusyanuar <bagus.yanuar613@gmail.com>
2025-04-28 14:08:23 +0700

    feat: install dropzone & vanillajs-datepicker

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 import './bootstrap';
+import {Datepicker} from 'vanillajs-datepicker';
+import Dropzone from 'dropzone';
+import 'dropzone/dist/dropzone.css';
+Dropzone.autoDiscover = false;
+window.Dropzone = Dropzone;
+window.Datepicker = Datepicker;
