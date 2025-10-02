9402a2f05d62a78f3310b46b082c205a7c008f62
Pradana Mahendra <pradanamahendra@gmail.com>
2025-10-02 21:37:19 +0700

    slick

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import laravel from 'laravel-vite-plugin'
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/sass/app.scss',
-                'resources/js/app.js',
-                'resources/js/util/index.js',
-                'resources/js/landing/index.js',
+                "resources/sass/app.scss",
+                "resources/js/app.js",
+                "resources/js/util/index.js",
+                "resources/js/landing/index.js",
+                "resources/js/util/slick.js",
             ],
             refresh: true,
         }),
     ],
-})
+});
