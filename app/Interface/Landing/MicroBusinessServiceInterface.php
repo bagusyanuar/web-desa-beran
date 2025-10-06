71ba8742be5df842c2a392101a75c7243d8c2a54
owlprojection <bagus.yanuar613@gmail.com>
2025-10-06 13:16:46 +0700

    store micro business

--- app/Interface/Landing/MicroBusinessServiceInterface.php
+++ app/Interface/Landing/MicroBusinessServiceInterface.php
@@ -3,8 +3,10 @@
 namespace App\Interface\Landing;
 
 use App\Commons\Libs\Http\ServiceResponse;
+use App\Schemas\Landing\MicroBusiness\MicroBusinessQuery;
 
 interface MicroBusinessServiceInterface
 {
+    public function findAll(MicroBusinessQuery $queryParams): ServiceResponse;
     public function findSome(): ServiceResponse;
 }
--- app/Services/Landing/MicroBusinessService.php
+++ app/Services/Landing/MicroBusinessService.php
@@ -2,12 +2,34 @@
 
 namespace App\Services\Landing;
 
+use App\Commons\Libs\Http\MetaPagination;
 use App\Commons\Libs\Http\ServiceResponse;
 use App\Interface\Landing\MicroBusinessServiceInterface;
 use App\Models\MicroBusiness;
+use App\Schemas\Landing\MicroBusiness\MicroBusinessQuery;
 
 class MicroBusinessService implements MicroBusinessServiceInterface
 {
+    public function findAll(MicroBusinessQuery $queryParams): ServiceResponse
+    {
+        try {
+            $queryParams->hydrateQuery();
+            $query = MicroBusiness::with(['owner', 'contact', 'image'])
+                ->when($queryParams->getParam(), function ($q) use ($queryParams) {
+                    /** @var Builder $q */
+                    return $q->where('title', 'LIKE', "%{$queryParams->getParam()}%")
+                        ->orWhereRelation('owner', 'name', 'LIKE', "%{$queryParams->getParam()}%");
+                })
+                ->orderBy('title', 'ASC');
+            $pagination = $query->paginate($queryParams->getPageSize(), '*', 'page', $queryParams->getPage());
+            $data = $pagination->items();
+            $meta = MetaPagination::generate($pagination);
+            return ServiceResponse::statusOK("successfully get micro business", $data, $meta);
+        } catch (\Throwable $e) {
+            return ServiceResponse::internalServerError($e->getMessage());
+        }
+    }
+
     public function findSome(): ServiceResponse
     {
         try {
