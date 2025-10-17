a750c47a0fef52089e337459f772e1d18da4e55e
owlprojection <bagus.yanuar613@gmail.com>
2025-10-17 22:08:55 +0700

    landing library

--- app/Livewire/Pages/Landing/Library/Detail.php
+++ app/Livewire/Pages/Landing/Library/Detail.php
@@ -2,10 +2,27 @@
 
 namespace App\Livewire\Pages\Landing\Library;
 
+use App\Services\Landing\LibraryService;
 use Livewire\Component;
+use Livewire\Attributes\Layout;
 
+
+#[Layout('layouts.app')]
 class Detail extends Component
 {
+    public $data;
+
+    public function mount($slug)
+    {
+        $service = new LibraryService();
+        $response = $service->findBySlug($slug);
+        if ($response->getStatus() === 200) {
+            $this->data = $response->getData();
+        } else {
+            $this->data = null;
+        }
+    }
+
     public function render()
     {
         return view('livewire.pages.landing.library.detail');
