9eb94f92176f7e5a1cda1da6aca994ac53ac92bf
owlprojection <bagus.yanuar613@gmail.com>
2025-10-21 14:06:51 +0700

    feat: sidebar

--- resources/views/livewire/layout/sidebar/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar/sidebar.blade.php
@@ -1,16 +1,17 @@
-<aside class="h-dvh w-72 fixed z-50">
-    <div class="w-full h-full flex flex-col bg-white border-r border-neutral-300 shadow-lg">
+<aside class="h-dvh w-72 fixed z-50 bg-brand-500">
+    <div class="w-full h-full flex flex-col bg-brand-500 border-r border-brand-500 shadow-lg shadow-brand-500">
         {{-- sidebar header --}}
-        <div class="w-full h-16 px-6 flex items-center">
-            <div class="w-full flex items-end gap-3">
-                <img src="{{ asset('static/images/logo-image.png') }}" alt="brand-logo" class="h-10" />
-                <span class="text-accent-500 font-bold"><span class="text-brand-500">Beran</span>Digital.id</span>
+        <div class="w-full h-24 px-6 flex items-center">
+            <div class="w-full flex flex-col">
+                {{-- <img src="{{ asset('static/images/logo-image.png') }}" alt="brand-logo" class="h-10" /> --}}
+                <span class="text-xs text-white mb-0 font-light">Platform Desa</span>
+                <span class="text-accent-500 font-bold text-lg"><span class="text-white">Beran</span>Digital.id</span>
             </div>
         </div>
 
         {{-- sidebar item --}}
-        <div class="w-full flex-1 flex flex-col gap-1 px-3 py-6 overflow-y-auto">
-            <x-sidebar.sidebar-item to="/dashboard" text="Dashboard" icon="house" />
+        <div class="w-full flex-1 flex flex-col gap-1 px-4 py-3 overflow-y-auto">
+            <x-sidebar.sidebar-item to="{{ route('web-panel.dashboard') }}" text="Dashboard" icon="house" />
             <x-sidebar.sidebar-item-tree text="Surat Online" icon="mailbox">
                 <x-sidebar.sidebar-item-tree-child to="{{ route('web-panel.online-letter.domicile') }}"
                     text="Domisili" />
