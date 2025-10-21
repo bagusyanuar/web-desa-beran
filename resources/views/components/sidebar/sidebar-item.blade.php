bc0af79afa25932ffc75075298d2a1f91c3a9631
owlprojection <bagus.yanuar613@gmail.com>
2025-10-21 15:21:43 +0700

    feat: sidebar

--- resources/views/components/sidebar/sidebar-item.blade.php
+++ resources/views/components/sidebar/sidebar-item.blade.php
@@ -1,17 +1,17 @@
 @props([
     'to' => '#',
     'text' => 'Item',
-    'icon' => 'circle'
+    'icon' => 'circle',
 ])
 
 @php
-    $isActive = request()->is(trim(parse_url($to, PHP_URL_PATH), '/').'*');
+    $isActive = request()->is(trim(parse_url($to, PHP_URL_PATH), '/') . '*');
 @endphp
 
 <a href="{{ $to }}"
-    class="group relative px-3 py-2.5 w-full rounded-md flex items-center gap-2 cursor-pointer {{ $isActive ? 'bg-accent-500' : 'hover:bg-accent-500' }} transition-all duration-300 ease-in-out"
-
-    >
-    <i data-lucide="{{ $icon }}" class="{{ $isActive ? 'text-white' : 'text-white/80 group-hover:text-white' }} h-5 aspect-[1/1] transition-all duration-300 ease-in-out"></i>
-    <span class="{{ $isActive ? 'text-white' : 'text-white/80 group-hover:text-white' }} font-light transition-all duration-300 ease-in-out">{{ $text }}</span>
+    class="group relative px-3 py-2.5 w-full rounded-md flex items-center gap-2 cursor-pointer {{ $isActive ? 'bg-accent-500' : 'hover:bg-accent-500' }} transition-all duration-300 ease-in-out">
+    <i data-lucide="{{ $icon }}"
+        class="{{ $isActive ? 'text-white' : 'text-white/80 group-hover:text-white' }} h-5 aspect-[1/1] transition-all duration-300 ease-in-out"></i>
+    <span
+        class="{{ $isActive ? 'text-white' : 'text-white/80 group-hover:text-white' }} font-light transition-all duration-300 ease-in-out">{{ $text }}</span>
 </a>
--- resources/views/livewire/layout/sidebar/sidebar.blade.php
+++ resources/views/livewire/layout/sidebar/sidebar.blade.php
@@ -3,7 +3,6 @@
         {{-- sidebar header --}}
         <div class="w-full h-24 px-6 flex items-center">
             <div class="w-full flex flex-col">
-                {{-- <img src="{{ asset('static/images/logo-image.png') }}" alt="brand-logo" class="h-10" /> --}}
                 <span class="text-xs text-white mb-0 font-light">Platform Desa</span>
                 <span class="text-accent-500 font-bold text-lg"><span class="text-white">Beran</span>Digital.id</span>
             </div>
@@ -12,7 +11,18 @@
         {{-- sidebar item --}}
         <div class="w-full flex-1 flex flex-col gap-1 px-4 py-3 overflow-y-auto">
             <x-sidebar.sidebar-item to="{{ route('web-panel.dashboard') }}" text="Dashboard" icon="house" />
-            <x-sidebar.sidebar-item-tree text="Surat Online" icon="mailbox">
+            <x-sidebar.sidebar-item-tree text="Surat Online" icon="mailbox" :routes="[
+                'web-panel.online-letter.domicile',
+                'web-panel.online-letter.death',
+                'web-panel.online-letter.birth',
+                'web-panel.online-letter.police-clearance',
+                'web-panel.online-letter.single-status',
+                'web-panel.online-letter.incapacity',
+                'web-panel.online-letter.income',
+                'web-panel.online-letter.widower',
+                'web-panel.online-letter.widow',
+                'web-panel.online-letter.parent-income',
+            ]">
                 <x-sidebar.sidebar-item-tree-child to="{{ route('web-panel.online-letter.domicile') }}"
                     text="Domisili" />
                 <x-sidebar.sidebar-item-tree-child to="{{ route('web-panel.online-letter.death') }}" text="Kematian" />
@@ -32,18 +42,29 @@
             </x-sidebar.sidebar-item-tree>
             <x-sidebar.sidebar-item to="{{ route('web-panel.micro-business') }}" text="Produk UMKM"
                 icon="shopping-bag" />
-            <x-sidebar.sidebar-item-tree text="Profil Desa" icon="info">
+            <x-sidebar.sidebar-item-tree text="Profil Desa" icon="info" :routes="[
+                'web-panel.history',
+                'web-panel.regional',
+                'web-panel.community',
+                'web-panel.potention',
+                'web-panel.staff',
+                'web-panel.vission-mission',
+            ]">
                 <x-sidebar.sidebar-item-tree-child to="{{ route('web-panel.history') }}" text="Sejarah Desa" />
                 <x-sidebar.sidebar-item-tree-child to="{{ route('web-panel.regional') }}" text="Wilayah Desa" />
                 <x-sidebar.sidebar-item-tree-child to="{{ route('web-panel.community') }}" text="Masyarakat Desa" />
                 <x-sidebar.sidebar-item-tree-child to="{{ route('web-panel.potention') }}" text="Potensi Desa" />
-                <x-sidebar.sidebar-item-tree-child to="{{ route('web-panel.vission-mission') }}" text="Visi dan Misi" />
+                <x-sidebar.sidebar-item-tree-child to="{{ route('web-panel.vission-mission') }}"
+                    text="Visi dan Misi" />
                 <x-sidebar.sidebar-item-tree-child to="{{ route('web-panel.staff') }}" text="Perangkat Desa" />
             </x-sidebar.sidebar-item-tree>
-            <x-sidebar.sidebar-item-tree text="Publikasi" icon="rss">
+            <x-sidebar.sidebar-item-tree text="Publikasi" icon="rss" :routes="[
+                'web-panel.news',
+                'web-panel.library',
+            ]">
                 <x-sidebar.sidebar-item-tree-child to="{{ route('web-panel.news') }}" text="Berita" />
                 <x-sidebar.sidebar-item-tree-child to="{{ route('web-panel.library') }}" text="Perpustakaan Online" />
-                <x-sidebar.sidebar-item-tree-child to="#" text="Galeri" />
+                {{-- <x-sidebar.sidebar-item-tree-child to="" text="Galeri" /> --}}
             </x-sidebar.sidebar-item-tree>
         </div>
     </div>
